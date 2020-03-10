b0b8459e0ca90ad3f40ddcb2c1983b31f5aaac94
Alex1899 <48056584+Alex1899@users.noreply.github.com>
2020-03-10 14:43:28 +0000

    main function

---
+++ Main.py
@@ -0,0 +1,90 @@
+import argparse
+import os
+import glob
+import subprocess
+from Parser import parse_frames
+from Evaluation import evaluate_form
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Exercise Form Evaluation')
+    parser.add_argument('--mode', type=str, default='evaluation')
+    parser.add_argument('--exercise', type=str, help='name of the exercise to evaluate ex. "bicep curl"')
+    parser.add_argument('--video_path', type=str, help='path to video to evaluate')
+    parser.add_argument('--videos_folder', type=str, default='videos', help='folder where all exercise videos are stored')
+    parser.add_argument('--keypoints_folder', type=str, default='keypoints_for_all', help='all keypoints folder')
+    parser.add_argument('--output_videos_folder', type=str, default='output_videos', help='output video folder in .avi')
+
+    arguments = parser.parse_args()
+    # os.chdir('openpose')
+
+    # extract keypoints from all videos in videos folder and store in keypoints_for_all folder.
+    if arguments.mode == 'keypoints_extraction':
+        if arguments.exercise:
+            video_folders = glob.glob(arguments.videos_folder + '/' + arguments.exercise)
+        else:
+            video_folders = glob.glob(arguments.videos_folder + '/*')
+
+        for vid_folder in video_folders:
+            exercise_videos = glob.glob(vid_folder + '/*')
+
+            for video in exercise_videos:
+                video_name = os.path.basename(video)
+                points_folder_name = str(video_name.split('.', 1)[0])
+                output_points_folder = os.path.join(arguments.keypoints_folder, vid_folder, str(points_folder_name))
+                print(output_points_folder)
+
+                if not os.path.exists(output_points_folder):
+                    os.makedirs(output_points_folder)
+
+                output_videos_folder = os.path.join(arguments.output_videos_folder, str(points_folder_name))
+                if not os.path.exists(output_points_folder):
+                    os.makedirs(output_videos_folder)
+                print(output_videos_folder)
+                output_video = output_points_folder + '/' + points_folder_name + '.avi'
+                openpose_demo = os.path.join('bin', 'OpenPoseDemo.exe')
+                """
+                subprocess.call([openpose_demo, '--video', video, '--write_json', output_points_folder, '--write_video',
+                                    output_video, '--number_people_max', '1'])
+                """
+
+    # if a specific video path is specified, evaluate that video and save keypoints/output video in the folder
+    elif arguments.mode == 'evaluation':
+        if arguments.video_path:
+            video_name = str(os.path.basename(arguments.video_path).split('.', 1)[0])
+            output_points_folder = os.path.join(arguments.keypoints_folder, arguments.exercise, video_name)
+            print(output_points_folder)
+
+            if not os.path.exists(output_points_folder):
+                os.makedirs(output_points_folder)
+
+            output_videos_folder = os.path.join(arguments.output_videos_folder, video_name)
+            if not os.path.exists(output_videos_folder):
+                os.makedirs(output_videos_folder)
+
+            print(output_videos_folder)
+            output_video = output_points_folder + '/' + video_name + '.avi'
+            openpose_demo = os.path.join('bin', 'OpenPoseDemo.exe')
+            """
+            subprocess.call([openpose_demo, '--video', arguments.video_path, '--write_json', output_points_folder, '--write_video',
+                             output_video, '--number_people_max', '1'])
+            """
+
+            frame_pose = parse_frames(output_points_folder)
+            evaluate_form(frame_pose, arguments.exercise, True)
+
+        else:
+            print('Please specify the video path to evaluate.')
+
+    else:
+        print('Wrong mode. Please use one of the followings: evaluation, keypoints_extraction.')
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
